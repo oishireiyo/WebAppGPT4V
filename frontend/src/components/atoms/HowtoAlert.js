4b1f619a1c15180776241fd19d125d1ec53ae5cf
Reiyo OISHI <reiyo.icepp@gmail.com>
2023-12-06 19:52:02 +0900

    Added new files which are used to show model outputs

---
+++ frontend/src/components/atoms/HowtoAlert.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Alert from '@mui/material/Alert'
+
+export default function() {
+  return (
+    <Alert severity="info">
+      質問と動画を設定してください。
+    </Alert>
+  )
+}
