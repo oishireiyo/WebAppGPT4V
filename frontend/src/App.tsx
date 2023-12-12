4eb5879027f82732bd89f48815ca4aa47fff9bb9
Reiyo OISHI <reiyo.icepp@gmail.com>
2023-12-12 23:36:49 +0900

    Updated Dockerfile and App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import Page from './components/templates/Page'
 import axios from 'axios'
 
 // https://keko5342.hatenadiary.jp/entry/2021/12/10/031756
-axios.defaults.baseURL = `http://127.0.0.1:5432`
+axios.defaults.baseURL = 'http://127.0.0.1:5432'
 
 export default function App() {
   return (
